cbbfcdfab19183e18ca70a855209e26f94fe2b9b
Przemek <przemek@da-sie.pl>
2025-03-10 23:46:15 +0100

    trait

--- src/Traits/WithAssistant.php
+++ src/Traits/WithAssistant.php
@@ -2,10 +2,21 @@
 
 namespace DaSie\Openaiassistant\Traits;
 
+use DaSie\Openaiassistant\Models\Assistant;
 use DaSie\Openaiassistant\Models\Thread;
+use Illuminate\Support\Str;
 
 trait WithAssistant
 {
+
+    public function newThread(Assistant $assistant)
+    {
+        return $this->threads()->create([
+            'assistant_id' => $assistant->id,
+            'uuid' => Str::uuid(),
+        ]);
+    }
+
     public function threads()
     {
         return $this->morphMany(Thread::class, 'model');
